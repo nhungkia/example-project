6662040f53721db3912bf57662065cf368353959
nhungkia <nhung.t.ho@gmail.com>
2014-01-21 14:24:21 -0800

    updated prompt in hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from hello.math import name_length, word_count
 
-name = raw_input("What's your name? ").strip()
+name = raw_input("Who are you? ").strip()
 
 while not name:
     name = raw_input("No really, what's your name? ").strip()
